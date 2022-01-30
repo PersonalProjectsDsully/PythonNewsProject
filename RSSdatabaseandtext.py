eae6d922b027d95b56e6d17d24e0f85c6b98dcb0
PersonalProjectsDsully <92706726+PersonalProjectsDsully@users.noreply.github.com>
2022-01-30 05:27:55 -0500

    Update RSSdatabaseandtext.py

--- RSSdatabaseandtext.py
+++ RSSdatabaseandtext.py
@@ -247,8 +247,8 @@ def TextNews():
 
 
 def telegram_bot_sendtext(bot_message):
-    bot_token = '2063309730:AAEZFGpeHViGtZ0dBhas7sGSuIo5qV3_0Q4'
-    bot_chatID = '2038236581'
+    bot_token = ''
+    bot_chatID = ''
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
